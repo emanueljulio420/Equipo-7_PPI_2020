69c93a98c0c50be74301864fa6d5c1c010c1fc61
simonecha <simon.echavarria.corre4@gmil.com>
2020-11-02 14:10:47 -0500

    se agregan nuevas peticiones de get en el backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const niveles = require('./routes/niveles');
+const profesor = require('./routes/profesor');
+const rendimineto = require('./routes/rendimiento');
+const subnivel = require('./routes/subnivel');
 
 app.set('port', 4001);
 
@@ -8,6 +12,9 @@ app.use(express.json());
 
 //rutas
 app.use('/api',routes);
+app.use('/api/niveles',niveles);
+app.use('/api/profesor',profesor);
+app.use('/api/subnivel',subnivel);
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
