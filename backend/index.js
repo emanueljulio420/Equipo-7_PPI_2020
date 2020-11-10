387c7d6a6d505345a234b5e65b1bfc426e0481a3
simonecha <simon.echavarria.corre4@gmil.com>
2020-11-10 14:48:24 -0500

    se borra el subniveles en el backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const routes = require('./routes/routes');
 const niveles = require('./routes/niveles');
 const profesor = require('./routes/profesor');
 const rendimineto = require('./routes/rendimiento');
-const subnivel = require('./routes/subnivel');
 
 app.set('port', 4001);
 
@@ -14,7 +13,7 @@ app.use(express.json());
 app.use('/api',routes);
 app.use('/api/niveles',niveles);
 app.use('/api/profesor',profesor);
-app.use('/api/subnivel',subnivel);
+
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
