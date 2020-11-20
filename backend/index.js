5f68a6985946445e00b1274c0f72d8c33bdf2bf4
simonecha <simon.echavarria.corre4@gmil.com>
2020-11-20 18:01:25 -0500

    se instala cors

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes/routes');
 const niveles = require('./routes/niveles');
 const profesor = require('./routes/profesor');
 const rendimineto = require('./routes/rendimiento');
 
-app.set('port', 4001);
+//importante para conexion del front con el back
+//Uso de cors
+app.use(cors({origin: '*'}));
+// conectar con el link de su frontend app.use(cors({origin: 'https://ppi-app.vercel.app/'}))
+app.set('port' ,process.env.PORT ||  4001);
 
 app.use(express.json());
+
 
 //rutas
 app.use('/api',routes);
 app.use('/api/niveles',niveles);
 app.use('/api/profesor',profesor);
+app.use('/api/rendimineto',rendimineto)
 
 
 app.listen(app.get('port'), () => {
