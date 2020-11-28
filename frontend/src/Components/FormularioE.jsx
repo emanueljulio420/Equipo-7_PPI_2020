5af43dada5cc6065e14afed185aa1ccdd7779844
simonecha <simon.echavarria.corre4@gmil.com>
2020-11-27 20:28:55 -0500

    se organiza algo en el formulario estudiante

--- frontend/src/Components/FormularioE.jsx
+++ frontend/src/Components/FormularioE.jsx
@@ -2,73 +2,80 @@ import React from 'react';
 import axios from "axios";
 import { Link } from 'react-router-dom';
 
-class FormularioEstuciante extends Component {
+class FormularioEstuciante extends React.Component {
+   
     constructor(props){
-     //es el lugar donde me traigo a las props y donde inicializo el estado
-     //1 es ir al constructor
-     super(props)
-     this.state={
-       datos: [],
-     
+      super(props)
+      this.state={
        form: {
-          id: '',
           nombre: '',
-          pais: '',
-          capital: ''
-       },
-       tipoModal: ''
-     }
-   } 
+          apellido: '',
+          correo: '',
+          contraseña:'',
+          colegio:'',
+          grado:'',
+          correo:''
 
-
-   peticionPost=async () =>{
+           
+        }
+      }
+    }
+ peticionPost=async () =>{
     delete this.state.form.id
      await axios.post('http://localhost:4001/api/estudiantes', this.state.form)
      .then(response =>{
-       this.modalInsertarForm();
-       this.peticionGet();
+       
+       
      }).catch(error=>{
       console.log(error.message);
     })
      
    }
-
-function () {
-    return (
+  
+  
+   handleChange=async(e)=>{
+    e.persist();
+    await this.setState({
+      form:{
+        ...this.state.form,
+       [e.target.name]: e.target.value
+      }
+    })
+    console.log(this.state.form)
+   }
+  
+ 
+  
+    render(){
+       const datosForm = this.state.form;
+       const daticos = this.state.datos;
+       console.log(daticos)
+      return (
 
         <div className="bg-white col-md-8 order-md-1 container my-5">
             <h1 className="mb-3 text-center">Registro</h1>
             <form className="needs-validation" novalidate="">
                 <div className="row">
                     <div className="col-md-6 mb-3">
-                        <label for="firstName">Nombres</label>
-                        <input type="text" className="form-control" id="Usuario" placeholder="Nombres" required=""></input>
+                        <label for="firstName">Nombre</label>
+                        <input type="text" className="form-control" id="Usuario" placeholder="Nombres"  onChange={this.handleChange} value={datosForm.nombre}></input>
                         <div className="invalid-feedback">Valid first name is required.</div>
                     </div>
                     <div className="col-md-6 mb-3">
                         <label for="lastName">Apellidos</label>
-                        <input type="text" className="form-control" id="Usuario" placeholder="Apellidos" required=""></input>
+                        <input type="text" className="form-control" id="Usuario" placeholder="Apellidos" required=""  onChange={this.handleChange} value={datosForm.apellido}></input>
                         <div className="invalid-feedback">Valid last name is required.</div>
                     </div>
                 </div>
-                <div className="mb-3">
-                    <label for="username">Nombre de usuario</label>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text">@</span>
-                        </div>
-                        <input type="text" className="form-control" id="Usuario" placeholder="Usuario" required=""></input>
-                        <div className="invalid-feedback" style={{ marginRight: 'width: 100%;' }}>Your username is required.</div>
-                    </div>
-                </div>
+
                 <div className="mb-3">
                     <label for="email">Contraseña</label>
-                    <input type="email" className="form-control" id="Contrasena"></input>
-                    <div className="invalid-feedback">Please enter a valid email address for shipping updates.</div>
+                    <input type="email" className="form-control" id="Contrasena" onChange={this.handleChange} value={datosForm.contraseña}></input>
+                    <div className="invalid-feedback" >Please enter a valid email address for shipping updates.</div>
                 </div>
                 <div className="mb-3">
                     <label for="email">Correo</label>
-                    <input type="email" className="form-control" id="email" placeholder="you@example.com"></input>
+                    <input type="email" className="form-control" id="email" placeholder="you@example.com" onChange={this.handleChange} value={datosForm.correo}></input>
                     <div className="invalid-feedback">Please enter a valid email address for shipping updates.</div>
                 </div>
                 <div className="row">
@@ -103,12 +110,13 @@ function () {
                 </div>
                 <hr className="mb-4"></hr>
                 <Link to="holae">
-                    <button className="btn btn-outline-danger btn-lg btn-block" type="submit"><font style={{ marginRight: 'vertical-align: inherit;' }}>Registrarme</font></button>
+                    <button className="btn btn-outline-danger btn-lg btn-block" type="submit" onClick={()=>this.peticionPost()}><font style={{ marginRight: 'vertical-align: inherit;' }}>Registrarme</font></button>
                 </Link>
             </form>
         </div>
-
+   
     );
-}
+ }
+}   
 
 export default FormularioEstuciante;
